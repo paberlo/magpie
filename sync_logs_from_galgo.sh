2212ffc387a7ef0a151059ea943ab4de56a3d47a
Pablo Bermejo López <pablo.bermejo@uclm.es>
2026-07-21 08:40:10 +0200

    remove obsolete sync/reproduce scripts, update IDE workspace state

--- sync_logs_from_galgo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-REMOTE_USER="pbermejo"
-REMOTE_HOST="galgo.uclm.es"
-REMOTE_BASE="/home/pbermejo/GeneticImprovement/magpie"
-LOCAL_BASE="/home/pablo/magpie"
-
-rsync -avz "$REMOTE_USER@$REMOTE_HOST:$REMOTE_BASE/_magpie_logs/" "$LOCAL_BASE/_magpie_logs/"
